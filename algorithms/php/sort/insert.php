faa8082b94b056a0f0d1f3aaa492456049e9a430
h2ero <122750707@qq.com>
2013-02-21 20:16:52 +0800

    format

--- algorithms/php/sort/insert.php
+++ algorithms/php/sort/insert.php
@@ -1,5 +1,19 @@
 <?php
 
+/* ------------------------------------------
+    Properties:
+    Stable
+    O(1) extra space
+    O(n^2) comparisons and swaps
+    Adaptive: O(n) time when nearly sorted
+    Very low overhead
+
+    -----------------------------------------
+    Although it is one of the elementary sorting algorithms with O(n2) worst-case time, insertion sort is the algorithm of choice either when the data is nearly sorted (because it is adaptive) or when the problem size is small (because it has low overhead).
+
+    For these reasons, and because it is also stable, insertion sort is often used as the recursive base case (when the problem size is small) for higher overhead divide-and-conquer sorting algorithms, such as merge sort or quick sort. 
+  --------------------------------------------*/
+
 $arr = array(1, 2, 0, 33, 5, 6);
 
 var_dump($arr);
@@ -8,12 +22,11 @@ $count = count($arr);
 
 for ($i = 1; $i < $count;$i ++ )
 {
-    for ($k = $i;  $k>0, $arr[$k] < $arr[$k - 1];  $k -- ) 
+    for ($k = $i; $k > 0, $arr[$k] < $arr[$k - 1]; $k-- ) 
     {
-        $swap    = $arr[$k];
-        $arr[$k] = $arr[$k-1];
-        $arr[$k-1] = $swap;
+        $temp      = $arr[$k];
+        $arr[$k]   = $arr[$k-1];
+        $arr[$k-1] = $temp;
     }
 }
-
 var_dump($arr);
