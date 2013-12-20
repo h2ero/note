cb4c5d62ce602eed96dbb9d868d1c6c17a5dfc6b
h2ero <122750707@qq.com>
2013-12-20 17:30:48 +0800

    Update upload_file.py

--- python/upload_file.py
+++ python/upload_file.py
@@ -6,7 +6,7 @@ def upload_file():
     session_id = "ecvd4a0u6jb2c4uuj1bjaknmi7"
     cookies = {'PMSID':'ecvd4a0u6jb2c4uuj1bjaknmi7'}
     files = {'userfile':open('c.txt', 'r')}
-    uploadurl = 'http://crm.int.jumeicd.com/CRMUser/upload'
+    uploadurl = 'http://xxx/upload'
     data = {'PMSID':session_id, '_ajax_':'true'}
     r = requests.post(uploadurl, files=files, data=data, cookies=cookies)
     print r.content
