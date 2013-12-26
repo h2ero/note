a0c67dabfc454484013523490841ce1cd9454fdb
h2ero <122750707@qq.com>
2013-12-26 11:36:02 +0800

    array post

--- python/upload_file.py
+++ python/upload_file.py
@@ -8,6 +8,8 @@ def upload_file():
     files = {'userfile':open('c.txt', 'r')}
     uploadurl = 'http://xxx/upload'
     data = {'PMSID':session_id, '_ajax_':'true'}
+    # 数组
+    # data = {'PMSID':session_id, '_ajax_':'true','sourceGroups[0][sql]':'ddd'}
     r = requests.post(uploadurl, files=files, data=data, cookies=cookies)
     print r.content
 
